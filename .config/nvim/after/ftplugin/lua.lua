2038556009ead53f18f96b9c5754d356297cfa58
Rok Mušič <rokmusic0@gmail.com>
2026-09-18 09:02:53 +0200

    Format lua files only when stylua is available

--- .config/nvim/after/ftplugin/lua.lua
+++ .config/nvim/after/ftplugin/lua.lua
@@ -4,6 +4,11 @@ vim.bo.formatprg = "stylua -"
 vim.api.nvim_create_autocmd("BufWritePre", {
 	buffer = 0,
 	callback = function()
+		if vim.fn.executable("stylua") == 0 then
+			vim.notify("stylua not found, skipping formatting.", vim.log.levels.WARN)
+			return
+		end
+
 		local view = vim.fn.winsaveview()
 		---@diagnostic disable-next-line: param-type-mismatch
 		local ok, err = pcall(vim.cmd, "%!stylua -")
